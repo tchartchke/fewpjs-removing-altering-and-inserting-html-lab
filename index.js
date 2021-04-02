ca66648666331ac555bbb4e5688699b7a2df7876
tchartchke <chartai.kl@gmail.com>
2021-04-01 20:25:27 -0500

    commit tree 4dae9b2a58d8c409c89d8c455342022c8d4c5f25

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+let main = document.querySelector("main")
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+
+let idName = 'victory'
+newHeader.id = idName
+
+newHeader.innerHTML = "is the champion"
+
+document.body.appendChild(newHeader)
